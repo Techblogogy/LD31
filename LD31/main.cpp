0c2a0abb120bfc07a28a133b300fa702c858e0eb
Fedor Bobylev <fedorbobylev@gmail.com>
2014-12-06 08:35:22 +0200

    LD31 v0.0
    Change Log:
    - Blank Project

--- LD31/main.cpp
+++ LD31/main.cpp
@@ -1,15 +1,16 @@
 //
-//  AppDelegate.h
+//  main.cpp
 //  LD31
 //
 //  Created by Fedor Bobylev on 12/6/14.
 //  Copyright (c) 2014 Fedor Bobylev. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#include <iostream>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
-
-
-@end
-
+int main(int argc, char** argv)
+{
+    
+    
+    return 0;
+}
