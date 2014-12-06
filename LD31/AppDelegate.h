6c625476117c5ca34607745247654f46095f71d2
Fedor Bobylev <fedorbobylev@gmail.com>
2014-12-06 08:30:04 +0200

    Initial Commit

---
+++ LD31/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LD31
+//
+//  Created by Fedor Bobylev on 12/6/14.
+//  Copyright (c) 2014 Fedor Bobylev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LD31/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LD31
+//
+//  Created by Fedor Bobylev on 12/6/14.
+//  Copyright (c) 2014 Fedor Bobylev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
